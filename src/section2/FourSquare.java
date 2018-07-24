34d93b201935c59d9f35a306df6b72d9eb8cdba0
Nico2006 <League@AdministorsiMac.attlocal.net>
2018-07-24 11:57:15 -0700

    Today
    We did four square and I made a cool object

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,27 +6,32 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+Robot Walee=new Robot("batman");
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		Walee.setSpeed(200);
 		// 5. Set the pen width to 5
-
+         Walee.setPenWidth(20);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+         for (int i=0; i<75; i++) {
 			// 7. Set the pen color to random
-	
+         Walee.setRandomPenColor();
 			// 1. Call the drawSquare() method
-	
+         drawSquare();
 			// 8. Turn the robot 90 degrees to the right
-
+        Walee.turn(70);
+         }
 	}
 
 	
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
-		/* 3. Fill in the code to draw a square inside the method below. */
 		
+		/* 3. Fill in the code to draw a square inside the method below. */   
+		Walee.penDown();
+		for (int i= 0; i<4; i++) {
+			Walee.move(100);
+			Walee.turn(90);
+		}
 		
 	}
 
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -7,9 +7,34 @@ public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
 		
-		// START HERE
-	
-		
-		
+		Robot Walee = new Robot("batman");
+		Walee.setSpeed(100);
+		Walee.sparkle();
+		Walee.penDown();
+		Walee.setSpeed(200);
+		Walee.move(100);
+		Walee.turn(90);
+	    Walee.penUp();
+		Walee.setSpeed(100);
+	    Walee.move(100);
+		Walee.turn(90);
+		Walee.penDown();
+		Walee.setSpeed(200);
+		Walee.move(100);
+		Walee.turn(90);
+		Walee.penUp();
+		Walee.move(200);
+		Walee.turn(90);
+	for(int i=0;i<2;i++) {Walee.move(100);
+	Walee.penDown();
+	Walee.turn(90);
+	for(int j=0;j<2;j++);
+	Walee.penUp();
+	Walee.turn(90);
 	}
+	
+	
+	}
+	
 }
+
